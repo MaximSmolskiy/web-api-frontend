8514160f8e0b3687bd85e2dd050a2870b2515f58
Smolskiy Maxim <mithridatus@mail.ru>
2021-04-15 16:11:44 +0300

    wip

--- DOM/modal.js
+++ DOM/modal.js
@@ -3,13 +3,15 @@ import {handleModalClick} from '../components/modal.js';
 export function getCreateFormValues() {
     const name = $('#createName').val();
     const surname = $('#createSurname').val();
-    const date = $('#createDate').val();
+    const date = $('#createDate').val().split('-').reverse().join('.');
     const position = $('#createPosition').val();
     const salary = Number($('#createSalary').val());
     return {name, surname, date, position, salary};
 }
 
 export function closeCreateForm() {
+    $('#createEmployee input').val('');
+    $('#createEmployee select').prop('selectedIndex', 0);
     $('#createEmployee').modal('hide');
 }
 
